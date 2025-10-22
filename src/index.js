dec34f8fcd01d53fc1c8acaea9cc9610ecdcba95
Sharif Ezzat <goodsharif@gmail.com>
2025-10-22 11:19:37 -0700

    updated arch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // JS Goes here - ES6 supported
 
-import "./css/main.css";
+import "./css/main.scss";
 
 var n = 0;
 var p = 0;
@@ -189,8 +189,8 @@ var tStart = $('#intro').scrollTop() // Start transition from top
   , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
 
 $(document).ready(function(){
-	$("body").css('background-color', "black");
-	$("body").css('color', "white");
+  $("body").css('background-color', "black");
+  $("body").css('color', "white");
     $(document).scroll(function() {
         var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
         p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
